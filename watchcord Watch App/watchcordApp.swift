122f30a9f636f3ee97bd4f40dfdcc1aa03829dda
circularsprojects <admin@circularsprojects.com>
2023-05-08 11:52:03 +1000

    Initial Commit

---
+++ watchcord Watch App/watchcordApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchcordApp.swift
+//  watchcord Watch App
+//
+//  Created by Levi Rigger on 8/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct watchcord_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ watchcord/watchcordApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchcordApp.swift
+//  watchcord
+//
+//  Created by Levi Rigger on 8/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct watchcordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
